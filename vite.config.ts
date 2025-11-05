6487505bd91b98e49aafb0238f20305560146e6a
Frain <frainworks1@mail.ru>
2025-11-05 17:11:05 +0300

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   build: {
-    outDir: 'dist',
+    outDir: 'build',
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
